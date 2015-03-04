07665315d059bd0c8a614d2e713eec0e2da9969f
DaoudDori <daoud.dori@hotmail.fr>
2015-03-04 14:32:06 +0100

    1ère modification
    c’est un test.

--- src/bean/BonjourBean.java
+++ src/bean/BonjourBean.java
@@ -9,15 +9,15 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class BonjourBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String            nom;
+	private String nom;
 
-    public String getNom() {
-        return nom;
-    }
+	public String getNom() {
+		return nom;
+	}
 
-    public void setNom( String nom ) {
-        this.nom = nom;
-    }
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
